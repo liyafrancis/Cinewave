5effa97e6eb06f9f4a458bd25f30644fc03b268c
lekshmi L <lekshmiiyyer123@gmail.com>
2024-08-18 23:39:06 +0530

    mm

--- frontend/src/Components/trial.jsx
+++ frontend/src/Components/trial.jsx
@@ -1,9 +1,13 @@
 history;
+<<<<<<< Updated upstream
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 export const trial = () => {
   return (
     <div>trial</div>
     
   )
 }
+=======
+
+>>>>>>> Stashed changes
